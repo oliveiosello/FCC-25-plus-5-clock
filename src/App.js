789752f86bf4e730b0beaa39c10fcedb0a79d82d
Olive Iosello <oiosello@gmail.com>
2020-11-13 19:51:29 -0800

    Added additional features

--- src/App.js
+++ src/App.js
@@ -1,51 +1,47 @@
-import React, { useEffect, useState } from "react";
-// import * as clock from "./clock"
+import React, { useEffect, useState, useRef } from 'react';
+
+const TimeControl = ({ id, name, onClick }) => (
+  <button className="button" id={id} onClick={onClick}>
+    {name}
+  </button>
+);
+
+
 
 const TimeButtons = ({ updateSessionLength, updateBreakLength }) => (
   <div className="session-buttons">
-    <button
-      className="button"
-      id="break-decrement"
-      onClick={() => updateBreakLength((length) => length + 1)}
-    >
-      B+
-    </button>
-
-    <button
-      className="button"
+    <TimeControl
+      name={'B+'}
       id="break-increment"
+      onClick={() => updateBreakLength((length) => length + 1)}
+    />
+    <TimeControl
+      name={'B-'}
+      id="break-decrement"
       onClick={() => updateBreakLength((length) => length - 1)}
-    >
-      B-
-    </button>
-
-    <button
-      className="button"
+    />
+    <TimeControl
+      name={'S+'}
       id="session-increment"
       onClick={() => updateSessionLength((length) => length + 1)}
-    >
-      S+
-    </button>
-
-    <button
-      className="button"
+    />
+    <TimeControl
+      name={'S-'}
       id="session-decrement"
       onClick={() => updateSessionLength((length) => length - 1)}
-    >
-      S-
-    </button>
+    />
   </div>
 );
 
 const ControlButtons = ({ toggle, isActive, resetClock }) => (
   <div className="control-buttons">
     <button className="button" id="start_stop" onClick={() => toggle()}>
-      {isActive ? "stop" : "start"}
+      {isActive ? 'stop' : 'start'}
     </button>
     <button className="button" id="reset" onClick={() => resetClock()}>
       reset
     </button>
-  </div>
+  </div>  
 );
 
 const BreakLength = ({ breakLength }) => {
@@ -67,10 +63,18 @@ const SessionLength = ({ sessionLength }) => {
 };
 
 const TimeLeft = ({ timeLeft }) => {
+  const totalSeconds = timeLeft;
+
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = (totalSeconds % 60).toString();
+  const paddedSeconds = seconds.length === 1 ? `0${seconds}` : seconds;
+
   return (
     <div id="timer-label">
       In Session
-      <div id="time-left">{timeLeft}</div>
+      <div id="time-left">
+        {minutes}:{paddedSeconds}
+      </div>
     </div>
   );
 };
@@ -79,20 +83,30 @@ const App = () => {
   const [sessionLength, setSessionLength] = useState(25);
   const [breakLength, setBreakLength] = useState(5);
   const [isActive, setIsActive] = useState(false);
+  const [elapsed, setElapsed] = useState(0);
 
-  const timeLeft = sessionLength + breakLength;
+  const interval = useRef();
 
-  const updateSessionLength = (cb) => {
-    setSessionLength(cb(sessionLength));
-  };
+  useEffect(() => {
+    if (isActive) {
+      interval.current = setInterval(() => {
+        setElapsed((elapsed) => elapsed + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval.current);
+      setElapsed(0);  
+    }
+  }, [isActive]);
 
-  const updateBreakLength = (cb) => {
-    setBreakLength(cb(breakLength));
-  };
+  const timeLeft = (sessionLength + breakLength) * 60 - elapsed;
 
-  const toggle = () => {
-    setIsActive(!isActive);
-  };
+  const minMax = (n, min, max) => Math.min(Math.max(n, min), max)
+
+  const updateSessionLength = (cb) => setSessionLength(minMax(cb(sessionLength),1, 60));
+
+  const updateBreakLength = (cb) => setBreakLength(minMax(cb(breakLength),1, 60));
+
+  const toggle = () => setIsActive(!isActive);
 
   const resetClock = () => {
     setSessionLength(25);
@@ -113,9 +127,13 @@ const App = () => {
       </div>
       <div className="time">
         <div>
-          <TimeLeft timeLeft={timeLeft} />
+          <TimeLeft timeLeft={timeLeft} id="time-left"/>
         </div>
-        <ControlButtons toggle={toggle} resetClock={resetClock} />
+        <ControlButtons
+          isActive={isActive}
+          toggle={toggle}
+          resetClock={resetClock}
+        />
       </div>
     </div>
   );
